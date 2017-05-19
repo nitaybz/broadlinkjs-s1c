e5d3a27039fee67a8a6f147fcf8984efeeb7ce3e
nitaybz <nitaybz@gmail.com>
2017-05-19 16:46:28 +0300

    Update s1c_check.js

--- test/s1c_check.js
+++ test/s1c_check.js
@@ -10,15 +10,19 @@ b.on("deviceReady", (dev) => {
     if (dev.type == "S1C") {
         console.log("S1C check power...");
         dev.get_sensors_status();
-
+        clearInterval(refresh);
         dev.on("power", (status_array) => {
             console.log("count is on " + status_array[0]);
             console.log("sensors is on " + status_array[1]);
         });
-
+        
     } else {
         console.log(dev.type + "@" + dev.host.address + " found... not MP1!");
         dev.exit();
     }
 
 });
+
+var refresh = setInterval(function(){
+    b.discover();
+}, 1000);
