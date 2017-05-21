76e0934188d721f89aeb8fce7acd5ecd548424ea
nitaybz <nitaybz@gmail.com>
2017-05-22 02:17:53 +0300

    Update s1c_check.js

--- test/s1c_check.js
+++ test/s1c_check.js
@@ -1,178 +1,36 @@
-var Accessory, Service, Characteristic, UUIDGen;
-var broadlink = require('broadlinkjs-s1c');
+'use strict';
+let broadlink = require('../../broadlinkjs-sm');
+let fs = require('fs');
 
-module.exports = function(homebridge) {
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    UUIDGen = homebridge.hap.uuid;
-    homebridge.registerPlatform("homebridge-broadlink-s1c", "broadlinkS1C", broadlinkS1C);
-}
+var b = new broadlink();
 
-function broadlinkS1C(log, config, api) {
-    this.log = log;
-    this.config = config;
-    this.ip = config.ip;
-    this.mac = config.mac;
-    if (api) {
-        this.api = api;
-    }
+b.discover();
 
-}
-
-broadlinkS1C.prototype = {
-    accessories: function(callback) {
-        //For each device in cfg, create an accessory!
-        var foundAccessories = this.config.accessories;
-        var myAccessories = [];
-
-        if (!this.ip && !this.mac) throw new Error("You must provide a config value for 'ip' or 'mac'.");
-
-        // MAC string to MAC buffer
-        this.mac_buff = function(mac) {
-            var mb = new Buffer(6);
-            if (mac) {
-                var values = mac.split(':');
-                if (!values || values.length !== 6) {
-                    throw new Error('Invalid MAC [' + mac + ']; should follow pattern ##:##:##:##:##:##');
-                }
-                for (var i = 0; i < values.length; ++i) {
-                    var tmpByte = parseInt(values[i], 16);
-                    mb.writeUInt8(tmpByte, i);
-                }
-            } else {
-                //this.log("MAC address emtpy, using IP: " + this.ip);
-            }
-            return mb;
-        }
-
-        for (var j = 0; j < foundAccessories.length; ij++){
-            var foundSensor = [{}];
-            var b = new broadlink();
-            b.discover();
-            b.on("deviceReady", (dev) => {
-                if (this.mac_buff(this.mac).equals(dev.mac) || dev.host.address == this.ip) {
-                    if (dev.type == "S1C") {
-                        dev.get_sensors_status();
-                        dev.on("sensors_status", (status_array) => {
-                            var count = status_array["count"];
-                            var sensors = status_array["sensors"];
-                            clearInterval(refresh);
-                            for (var i = 0; i < count; i++) {
-                                if (sensors[i].type == ("Motion Sensor" || "Door Sensor")) {
-                                    foundSensor[i].sensorName = sensors[i].name;
-                                    foundSensor[i].serial = sensors[i].serial;
-                                    foundSensor[i].type = sensors[i].type;
-                                    var accessory = new BroadlinkSensor(this.log, foundSensor[i]);
-                                    myAccessories.push(accessory);
-                                    this.log('Created ' + accessory.name + accessory.type +' Named: ' + accessory.sensorName);
-                                }
-                            }
-                            callback(myAccessories);
-                        });
-                    } else {
-                        console.log(dev.type + "@" + dev.host.address + " found... not S1C!");
-                        dev.exit();
-                    }
-                }
-            });
-            var refresh = setInterval(function(){
-                b.discover();
-            }, 1000);
-        }
-        
-    }
-}
-
-function BroadlinkSensor(log, config) {
-    this.log = log;
-    this.config = config;
-    this.sname = config.sensorName || "";
-    this.serial = config.serial || "";
-    this.type = config.type;
-    this.name = config.name + +"_"+ this.sname;
-    this.ip = config.ip;
-    this.mac = config.mac;
-    this.detected = false;
-
-    if (!this.ip && !this.mac) throw new Error("You must provide a config value for 'ip' or 'mac'.");
-
-    // MAC string to MAC buffer
-    this.mac_buff = function(mac) {
-        var mb = new Buffer(6);
-        if (mac) {
-            var values = mac.split(':');
-            if (!values || values.length !== 6) {
-                throw new Error('Invalid MAC [' + mac + ']; should follow pattern ##:##:##:##:##:##');
-            }
-            for (var i = 0; i < values.length; ++i) {
-                var tmpByte = parseInt(values[i], 16);
-                mb.writeUInt8(tmpByte, i);
-            }
-        } else {
-            //this.log("MAC address emtpy, using IP: " + this.ip);
-        }
-        return mb;
-    }
-}
-
-BroadlinkSensor.prototype = {
-    getServices: function() {
-        var services = [];
-        var informationService = new Service.AccessoryInformation();
-        informationService
-            .setCharacteristic(Characteristic.Manufacturer, 'Broadlink S1C')
-            .setCharacteristic(Characteristic.Model, this.type)
-            .setCharacteristic(Characteristic.SerialNumber, this.serial);
-        if (this.type == "Motion Sensor"){
-            var MotionhService = new Service.MotionSensor(this.name, UUIDGen.generate(this.serial));
-            MotionhService
-                .getCharacteristic(Characteristic.MotionDetected)
-                .on('get', this.getState.bind(this));
-        } else if (this.type == "Door Sensor"){
-            var DoorService = new Service.ContactSensor(this.name, UUIDGen.generate(this.serial));
-            DoorService
-                .getCharacteristic(Characteristic.ContactSensorState)
-                .on('get', this.getState.bind(this));
-        }
-        
-        services.push(switchService, informationService);
-
-        return services;
-    },
-
-    getState: function(callback) {
-        var self = this;
-        var b = new broadlink();
-        b.discover();
-
-        b.on("deviceReady", (dev) => {
-            if (self.mac_buff(self.mac).equals(dev.mac) || dev.host.address == self.ip) {
-                dev.get_sensors_status();
-                dev.on("sensors_status", (status_array) => {
-                    var sensors = status_array["sensors"];
-                    var count = status_array["count"];
-                    dev.exit();
-                    clearInterval(checkAgain);
-                    self.log(self.name + self.sname + " power is on - " + pwr);
-                    for (var i=0; i<count; i++){
-                        if (self.serial == sensors[i].serial){
-                            if (sensors[i].status = 0) {
-                            self.detected = false;
-                            return callback(null, false);
-                        } else {
-                            self.detected = true;
-                            return callback(null, true);
-                        }
-                        }
-                    }
-                });
-            } else {
-                dev.exit();
+b.on("deviceReady", (dev) => {
+    if (dev.type == "S1C") {
+        console.log("S1C check power...");
+        dev.get_sensors_status();
+        clearInterval(refresh);
+        dev.on("sensors_status", (status_array) => {
+            
+            var count = status_array["count"];
+            var sensors = status_array["sensors"]
+            console.log("How Many Sensors? " + count);
+            for (var i=0; i<count; i++){
+                console.log("Sensor #"+(i+1)+" Name: " + sensors[i].name);
+                console.log("Sensor #"+(i+1)+" Type: " + sensors[i].type);
+                console.log("Sensor #"+(i+1)+" Status: " + sensors[i].status);
+                console.log("Sensor #"+(i+1)+" Serial: " + sensors[i].serial);
             }
         });
-        var checkAgain = setInterval(function() {
-            b.discover();
-        }, 1000)
-
+        
+    } else {
+        console.log(dev.type + "@" + dev.host.address + " found... not MP1!");
+        dev.exit();
     }
-}
+
+});
+
+var refresh = setInterval(function(){
+    b.discover();
+}, 1000);
